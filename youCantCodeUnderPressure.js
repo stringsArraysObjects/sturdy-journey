75d0f1fafbf180737e0e8c4f142c3b823e5dc63f
Chris <chtuff@gmail.com>
2023-12-14 11:35:58 -0500

    warming up

---
+++ youCantCodeUnderPressure.js
@@ -0,0 +1,13 @@
+//PARAMETERS: Code as fast as you can
+//RESULT: double the integer and return it
+//EXAMPLE: Test.assertEquals(doubleInteger(2), 4);
+//PSEUDOCODE: double a number.
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+  }
+  function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i+i;
+  }
+  const doubleInteger = i => 2 * i;
