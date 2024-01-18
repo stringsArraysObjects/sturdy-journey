30c8f340d3deecd45af0dabc9e2982481449df43
Chris <chtuff@gmail.com>
2024-01-18 12:56:30 -0500

    solution

---
+++ stringyString.js
@@ -0,0 +1,18 @@
+//PARAMETER: Given an integer size
+//           return a string
+//RESULT: Write a function that takes a
+//          integer size and returns a sequence
+//          of that size of 1s and 0s.
+//EXAMPLE: A string with the size 6 return: '101010'
+//PSEUDOCODE: create a loop that repeats as
+//             many times as the given size integer
+//             concat an empty string with 1s or 0s
+//             during each interaction of the loop
+function stringy(size) {
+
+    let result = '';
+  for (let i = 1; i <= size; i++) {
+    result += i % 2;
+  }
+  return result;
+} 
