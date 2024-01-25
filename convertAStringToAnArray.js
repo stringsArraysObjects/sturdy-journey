fa80cd1e4a43f5b0edd80d15e092b865795085bf
Chris <chtuff@gmail.com>
2024-01-24 19:10:38 -0500

    solution

---
+++ convertAStringToAnArray.js
@@ -0,0 +1,13 @@
+/*PARAMETERS: words and sentences
+*             no sorting needed
+*RESULT:    Write a function to split a string and
+*           convert it into an array of words.
+*EXAMPLE:  "I love arrays they are my favorite" 
+            ==> 
+            ["I", "love", "arrays", "they", "are", "my", "favorite"]
+*PSEUDOCODE: split the string into an array by the given delimiter, being quotes.
+
+*/
+function stringToArray(string){
+    return string.split('');
+  }
