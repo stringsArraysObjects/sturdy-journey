07d3a81c7d52fc8e30dd5c80366b01e8ced232c7
Chris <chtuff@gmail.com>
2024-01-26 11:58:31 -0500

    solution

---
+++ convertAStringToANumber.js
@@ -0,0 +1,14 @@
+//PARAMETER: all inputs will be strings,
+//           and every string is a perfectly 
+//           valid representation of an integral number
+//RESULT:   We need a function that can transform a string into a number
+//EXAMPLE:  "1405" --> 1405
+//PSEUDOCODE: change the data type from a string input to an integer output
+
+const stringToNumber = function(str){
+    return Number(str);
+  }
+
+const stringToNumber = function(str){
+    return str * 1;
+  }
