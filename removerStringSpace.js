223fb11c2d0e443dc5919d3f5ac76ac4ccd0ee3d
Chris <chtuff@gmail.com>
2024-01-26 00:15:01 -0500

    solution

---
+++ removerStringSpace.js
@@ -0,0 +1,7 @@
+//PARAMETER: 
+//RESULT:   Write a function that removes the 
+//          spaces from the string, then return 
+//          the resultant string.
+//EXAMPLE: Input -> Output
+//         "8aaaaa dddd r     " -> "8aaaaaddddr"
+//PSEUDOCODE: remove the whitespace
