74d23da4389b068d7d9c215b008adfc2882ccc33
Chris <chtuff@gmail.com>
2024-02-02 17:12:45 -0500

    n/a

---
+++ removeTheMinimum.js
@@ -0,0 +1,11 @@
+//PARAMETERS: do not mutate the original array/list
+//              if there are multiple elements with
+//              the same value, remove the one with
+//              a lower index. If you get an empty
+//              array/list, return an empty array/list
+//              don't change the order of the elements
+//              that are left.
+//RESULT:       given an array of integers, remove the smallest
+//              value.
+//EXAMPLE:      Input: [2,2,1,2,1], output = [2,2,2,1]
+//
