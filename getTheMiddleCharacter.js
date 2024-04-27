ef0cfacf41a6e36039668f73976ef12a4fbf05e4
Mason <chtuff@gmail.com>
2024-04-27 11:33:50 -0400

    solution

--- getTheMiddleCharacter.js
+++ getTheMiddleCharacter.js
@@ -2,4 +2,9 @@
 //RESULT:   If the word's length is odd, return the middle character. 
 //          If the word's length is even, return the middle 2 characters.
 //EXAMPLE:  getMiddle("test") should return "es"
-//PSEUDOCODE:     
+//PSEUDOCODE:   str.substr(start, length)
+
+function getMiddle(s)
+{
+  return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+}
