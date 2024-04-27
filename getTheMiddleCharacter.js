8dd07be58f9fa467a23e8a8fbc39db607bfe1a1e
Mason <chtuff@gmail.com>
2024-04-27 11:31:35 -0400

    solution

---
+++ getTheMiddleCharacter.js
@@ -0,0 +1,5 @@
+//PARAMETERS:
+//RESULT:   If the word's length is odd, return the middle character. 
+//          If the word's length is even, return the middle 2 characters.
+//EXAMPLE:  getMiddle("test") should return "es"
+//PSEUDOCODE:     
