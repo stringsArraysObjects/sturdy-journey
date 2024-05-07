957546e7b68c84cf435ee6ac89149b1229aca383
Mason <chtuff@gmail.com>
2024-05-07 10:30:32 -0400

    solution

---
+++ isDivisible.js
@@ -0,0 +1,7 @@
+//PARAMETERS:   Inputs are always positive and > 0
+//RESULT:       create a function that checks if a number n
+//              is divisible by two numbers x and y.
+//              return (true/false)
+//EXAMPLE:      1) n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
+//PSEUDOCODE:   use modulus, equality operators, and conditional operators for short circuit behavior
+
