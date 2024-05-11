853b92f1a4f214ba5e5b19e8a3e979630ac8fdfb
Chris <chtuff@gmail.com>
2024-05-10 20:19:52 -0400

    solution

--- map.js
+++ map.js
@@ -2,7 +2,7 @@
 //RESULT:       given an array of integers, return
 //              a new array with each value doubled
 //EXAMPLE:      [1, 2, 3] --> [2, 4, 6]
-//PSEUDOCODE:   Will use the map The map() method of 
+//PSEUDOCODE:   Will use the map() method of 
 //              Array instances creates a new array populated 
 //              with the results of calling a provided function
 //              on every element in the calling array.
