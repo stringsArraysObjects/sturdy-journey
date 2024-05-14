e3bb4b343dda181911418cf84614bbb58f78341c
Mason <chtuff@gmail.com>
2024-05-14 19:55:25 -0400

    solution

--- reduceMyFraction.js
+++ reduceMyFraction.js
@@ -5,3 +5,10 @@
 //            output:  [reduced numerator, reduced denominator]
 //            example: [45, 120] --> [3, 8]
 //
+
+const gcd = (a, b) => b ? gcd(b, a % b) : a;
+
+const reduce = ([a, b]) => {
+  let d = gcd(a, b);
+  return [a / d, b / d];
+};
