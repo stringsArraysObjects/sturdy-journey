e49612a250987d2130648b57f1910502d73f7b9f
Mason <chtuff@gmail.com>
2024-05-14 19:37:11 -0400

    solution

---
+++ reduceMyFraction.js
@@ -0,0 +1,7 @@
+//PARAMETERS: only positive numbers
+//RESULT:     write a function that reduces fractions to their
+//            simplest form.
+//EXAMPLE:    input:   [numerator, denominator]
+//            output:  [reduced numerator, reduced denominator]
+//            example: [45, 120] --> [3, 8]
+//
