ef5c18a4dcca66af205cf43762cb984e1243073e
Mason <chtuff@gmail.com>
2024-05-16 19:28:43 -0400

    solution

--- binaryString.js
+++ binaryString.js
@@ -2,4 +2,15 @@
 //RESULT:       Given a positive (or 0) number, 
 //              return a string of 1's and 0's 
 //              representing it's binary value 
-//EXAMPLE:        
+//EXAMPLE:      toBinaryString(6)
+//              should return "110" (no leading 0). 
+   
+
+function toBinaryString(number){
+    var r = '';
+    do {
+      r = (number & 1) + r;
+      number >>= 1;
+    } while(number);
+    return r;
+  }
