795e3c353ebf866ad2584bc0d282ed04bfa852fb
Mason <chtuff@gmail.com>
2024-05-16 19:27:43 -0400

    solution

---
+++ binaryString.js
@@ -0,0 +1,5 @@
+//PARAMETER:    toString not allowed
+//RESULT:       Given a positive (or 0) number, 
+//              return a string of 1's and 0's 
+//              representing it's binary value 
+//EXAMPLE:        
