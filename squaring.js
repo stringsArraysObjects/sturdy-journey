2312ea27d4239169e30fc2533c1767c273ed4299
Mason <chtuff@gmail.com>
2024-05-22 18:02:16 -0400

    solution

---
+++ squaring.js
@@ -0,0 +1,12 @@
+//PARAMETERS:
+//RESULT:   Now you have to write a function that takes an 
+//          argument and returns the square of it. 
+//EXAMPLE:  
+//PSEUDOCODE:   write a classic function that 
+//              takes in a number as a parameter
+//              return the square of that number
+//              by utilizing the Math.pow static method.
+//              
+function square(num) {
+    return Math.pow(num, 2);
+ }
