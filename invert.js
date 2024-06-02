b05c547ea8debfffb05526b2bb006a5241d5e6c5
Chris <chtuff@gmail.com>
2024-06-02 17:28:37 -0400

    solution

---
+++ invert.js
@@ -0,0 +1,6 @@
+// return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// [1, 2, 3, 4, 5] --> [-1, -2, -3, -4, -5]
+// [1, -2, 3, -4, 5] --> [-1, 2, -3, 4, -5]
+// [] --> []
+const invert = array => array.map(num => -num);
