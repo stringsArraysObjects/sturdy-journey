241fa23d2da43e003c604db73a824076f28998d8
Chris <chtuff@gmail.com>
2024-06-09 18:46:17 -0400

    solution

--- sumMix.js
+++ sumMix.js
@@ -1,7 +1,6 @@
 //PARAMETER:
 //RESULT:  return the sum of the array values as if all were numbers.
 //EXAMPLE: [9, 3, '7', '3']), 22);
-
 //PSUEDOCODE:   
 //              use the reduce method to combine each element by addition and the number method
 //              to covert the strings to integers and return a single value.
@@ -9,8 +8,7 @@
 //              use the map method to call the Number function for each element of the array 
 //              and return an array of all numbers that will be added together by using the reduce 
 //              method.
-//
-//
+
 
 
 function sumMix(x){
